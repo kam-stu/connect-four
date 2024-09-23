f29b45b4bfee409d590457d834efb5562968ef79
aintkam <kstuckey817@gmail.com>
2024-09-22 20:45:33 -0500

    Board improvements
    - Fixed changeVal() not working as intended
    - Added isFull() to check whether the board is full or not (if there's any remaining moves left)
    - Added template functions: checkWin(), getWinner, switchPlayer(), checkMove() for further implementation

--- Board.java
+++ Board.java
@@ -2,13 +2,15 @@ public class Board {
     private int rows;
     private int columns;
     public char[][] board;
+    private boolean isDraw;
 
     public Board(){
         this.rows = 6;
         this.columns = 7;
         this.board = new char[rows][columns];
+        this.isDraw = false;
     }
-
+    
     public void initBoard(){
         for(int row = 0; row < board.length; row++){
             for(int col = 0; col < board[row].length; col++){
@@ -16,7 +18,7 @@ public class Board {
             }
         }
     }
-
+    
     public void printBoard(){
         for(int row = 0; row < board.length; row++){
             for(int col = 0; col < board[row].length; col++){
@@ -29,5 +31,51 @@ public class Board {
     // Changes the lowest point in the column to value
     // Off-by-one error for col parameter
     public void changeVal(int col, char value){
+        // Start from the bottom of the board and go up
+        for(int row = board.length - 1; row >= 0; row--){
+            if(board[row][col - 1] == '_'){
+                board[row][col - 1] = value;
+                return;
+            }
+        }
+    }
+
+    // Checks whether the board is filled or not
+    // If the entire top row is full, then the board is filled
+    public boolean isFull(){
+        for(int currentCol = 0; currentCol < board[0].length; currentCol++){
+            if(board[0][currentCol] == '_'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Checks whether there is a legal move
+    // Returns boolean whether there is a move
+    public boolean checkMove(int col){
+        // Checks if given column is a valid number
+        if(col <= 0 || col > columns){
+            System.out.println("Enter a valid column.");
+            return false;
+        }
+        return true;
+    }
+
+    // Checks to see if 4 of the same values are next to each other in the array
+    // Values can be next to each other horizontally, vertically, or diagonally
+    // Returns the value 
+    public String checkWin(){
+        return "None";  // If no winner is found
+    }
+
+    // Returns the winner or Draw
+    public String getWinner(){
+        return "Draw";
+    }
+
+    // Changes the "player" or x/o value
+    public void switchPlayer(){}
+
 
 }
--- main.java
+++ main.java
@@ -5,8 +5,28 @@ public class Main {
         Board board = new Board();
         board.initBoard();
         board.printBoard();
-        board.changeVal(1, 'x');
         System.out.println();
-        board.printBoard();
+        for(int i = 0; i < board.board.length; i++){
+            board.changeVal(1, 'x');
+            board.changeVal(2, 'x');
+            board.changeVal(3, 'x');
+            board.changeVal(4, 'x');
+            board.changeVal(5, 'x');
+            board.changeVal(6, 'o');
+            
+            board.printBoard();
+            System.out.println();
+        }
+        for (int i = 0; i < board.board.length - 1; i++){
+            board.changeVal(7, 'x');
+            board.printBoard();
+            System.out.println();
+        }
+
+        boolean result = board.isFull();
+        System.out.println(result); // Should be false
+        board.changeVal(7, 'x');
+        result = board.isFull();
+        System.out.println(result); // Should be true
     }
 }
